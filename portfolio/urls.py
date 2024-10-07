fa3a522807c74ef37d3854d8eba7d0a1d40a3ba4
SkylarYHU <skylarhyn@gmail.com>
2024-10-07 10:12:00 +0100

    modify urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('portfolioapp.urls')),
 ]
+
+# 添加媒体文件和静态文件的 URL 配置（仅用于开发环境）
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,
