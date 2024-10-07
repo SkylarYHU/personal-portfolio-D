72345c31579babf5b497ad53786c33d2e29448a2
SkylarYHU <skylarhyn@gmail.com>
2024-10-07 10:05:37 +0100

    modify urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,16 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('portfolioapp.urls'))
-] + static(settings.MEDIA_URL, settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
-  + static(settings.STATIC_URL, settings.MEDIA_URL,
-           document_root=settings.STATIC_ROOT)
+    path('', include('portfolioapp.urls')),
+]
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,
