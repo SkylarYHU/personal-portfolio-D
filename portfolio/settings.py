abb0576183006de65bff4a7e91542c5eb465d1b0
Skylar <skylarhyn@gmail.com>
2025-09-15 02:00:52 +0100

    Fix static files configuration - unify settings for dev and production to prevent image display issues

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,21 +123,16 @@ if not DEBUG:
         conn_max_age=600, ssl_require=True)
 
 # Static files (CSS, JavaScript, Images)
-if DEBUG:
-    # Local development settings for static and media files
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-else:
-    # Production settings for static and media files
-    STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    
-    # Simplified static file serving with whitenoise
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:
+    # Production settings for static file serving with whitenoise
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
